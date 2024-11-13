3cc9080cd53e6d9ec92521fd498138d7ce44da74
anila <anilabano29161721@gmail.com>
2024-11-13 21:01:48 +0500

    Initial commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,45 +1,86 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion, useInView } from 'framer-motion'
+import { useRef } from 'react';
+
 
 const About = () => {
-  return (
-    <>
-     <div className=" mx-auto flex flex-col items-center justify-center p-10 md:px-10 lg:px-20 w-full overflow-hidden py-10  md:py-20 space-y-2 bg-[#fbfbfc]" id="About">
-<h1 className='text-2xl sm:text-4xl md:5xl font-semibold capitalize'> About <span className='underline underline-offset-4 decoration-1 under font-light'>our brand</span></h1>
-<p className='text-gray-800 text-center'>
-    passinate about propertise didicated bout vision
-</p>
-<div className="flex flex-col md:flex-row items-start md:items-center md:gap-20 py-16">
-<img src={assets.brand_img} alt="" className='w-full md:w-1/2'/>
-<div className="flex flex-col items-center justify-center md:items-start mt-10 text-gray-600 ">
-<div className="grid grid-cols-2 gap-6 w-full  ">
-<div className='text-3xl font-medium text-gray-800'>
-    <p>+10</p>
-    <p className='text-sm'>year of experiance</p>
-</div>
-<div className='text-3xl font-medium text-gray-800'>
-    <p>+10</p>
-    <p className='text-sm'>project completed</p>
-</div>
-<div className='text-3xl font-medium text-gray-800'>
-    <p>+10</p>
-    <p  className="text-sm">mn.fq.sm Develivery</p>
-</div>
-<div className='text-3xl font-medium text-gray-800'>
-    <p>+10</p>
-    <p className='text-sm'>ongoing project</p>
-</div>
-</div>
-<div>
-<p className='py-5 text-gray-800 capitalize text-sm leading-6 tracking-wide font-light'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, voluptatibus? Sequi, dicta quod. Earum rem assumenda quas aspernatur, repellendus modi!</p>
-<a href='#Contact' className='bg-gray-700 text-white py-2 px-7 capitalize rounded-sm'>Contact us</a>
 
-</div>
-</div>
-</div>
-     </div> 
-    </>
-  )
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true }); // once: true means animation triggers only once
+
+    return (
+        <>
+            <div className=" mx-auto flex flex-col items-center justify-center p-10 md:px-10 lg:px-20 w-full overflow-hidden py-10  md:py-20 space-y-2 bg-[#fbfbfc]" id="About">
+                <h1 className='text-2xl sm:text-4xl md:5xl font-semibold capitalize'> About <span className='underline underline-offset-4 decoration-1 under font-light'>our brand</span></h1>
+                <p className='text-gray-800 text-center'>
+                    passinate about propertise didicated bout vision
+                </p>
+                <div className="flex flex-col md:flex-row items-start md:items-center md:gap-20 py-16">
+                    <motion.img
+                        ref={ref}
+                        initial={{ x: -100 }}
+                        animate={isInView ? { x: 0 } : {}}
+                        transition={{ duration: 2 }}
+
+                        src={assets.brand_img} alt="" className='w-full md:w-1/2' />
+                    <div className="flex flex-col items-center justify-center md:items-start mt-10 text-gray-600 ">
+                        <div className="grid grid-cols-2 gap-6 w-full  ">
+                            <motion.div
+                                ref={ref}
+
+                                initial={{ y: 50 }}
+                                animate={isInView ? { y: 0 } : {}}
+                                transition={{ duration: 1, delay: 0.2 }}
+                                className='text-3xl font-medium text-gray-800'>
+                                <p>+10</p>
+                                <p className='text-sm'>year of experiance</p>
+                            </motion.div>
+                            <motion.div
+                                ref={ref}
+                                initial={{ y: 50, opacity: 0 }}
+                                animate={isInView ? { y: 0, opacity: 1 } : {}}
+                                transition={{ duration: 1, delay: 0.5 }} className='text-3xl font-medium text-gray-800'>
+                                <p>+10</p>
+                                <p className='text-sm'>project completed</p>
+                            </motion.div>
+                            <motion.div
+                            ref={ref}
+                                initial={{ y: 50, opacity: 0 }}
+                                animate={isInView?{ y: 0, opacity: 1 }:{}}
+                                transition={{ duration: 1, delay: 0.8 }} className='text-3xl font-medium text-gray-800'>
+                                <p>+10</p>
+                                <p className="text-sm">mn.fq.sm Develivery</p>
+                            </motion.div>
+                            <motion.div
+                            ref={ref}
+                                initial={{ y: 50, opacity: 0 }}
+                                animate={isInView?{ y: 0, opacity: 1 }:{}}
+                                transition={{ duration: 1, delay: 0.9 }} className='text-3xl font-medium text-gray-800'>
+                                <p>+10</p>
+                                <p className='text-sm'>ongoing project</p>
+                            </motion.div>
+                        </div>
+                        <div>
+                            <motion.p
+                            ref={ref}
+                            initial={{y:50,opacity:0}}
+                            animate={isInView?{y:0, opacity:1}:{}}
+                            transition={{duration:1,delay:1}}
+                             className='py-5 text-gray-800 capitalize text-sm leading-6 tracking-wide font-light'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, voluptatibus? Sequi, dicta quod. Earum rem assumenda quas aspernatur, repellendus modi!</motion.p>
+                            <motion.a
+                               ref={ref}
+                            initial={{y:50,opacity:0}}
+                            animate={isInView?{y:0, opacity:1}:{}}
+                            transition={{duration:1,delay:1.2}}
+                             href='#Contact' className='bg-gray-700 text-white py-2 px-7 capitalize rounded-sm'>Contact us</motion.a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default About
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,23 +2,49 @@ import React from 'react'
 import Navbar from './Navbar'
 // import gsap from "gsap";
 // import { useGSAP } from "@gsap/react";
-// import {Motion, spring} from 'react-motion';
+import { delay, motion } from "framer-motion"
 
+const fadeInUpAnimation = {
+    hidden: {
+        opacity: 0,
+        y: 100
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 2,
+        }
+    }
+}
 const Header = () => {
 
- 
     return (
         <>
-         
+
             <div className='images' id="Header">
                 <Navbar />
                 <div className='mx-auto text-center flex justify-center items-center flex-col py-4 px-6 md:px-50 space-y-3'>
-                    <h2 
-                  
-                     className='text-4xl md:text-4xl lg:text-5xl text-white uppercase font-semibold max-w-3xl pb-5'>EXPLore home that fit you dreams</h2>
+                    <motion.h2
+                        initial="hidden"
+                        animate="show"
+                        variants={fadeInUpAnimation}
+                        className='text-4xl md:text-4xl lg:text-5xl text-white uppercase font-semibold max-w-3xl pb-5'>EXPLore home that fit you dreams</motion.h2>
                     <div className='gap-4 flex justify-center items-center capitalize '>
-                        <a href="https://anilaimamkhan.github.io/Anila_Portfolio/" className='px-5 py-2 rounded-full bg-black text-white border' >My project </a>
-                        <a href="#Contact" className='px-5 py-2 bg-transparent border border-white rounded-full text-white'>contact</a>
+                        <motion.a 
+                           initial="hidden"
+                        animate="show"
+                        variants={fadeInUpAnimation}
+                        href="https://anilaimamkhan.github.io/Anila_Portfolio/" className='px-5 py-2 rounded-full bg-black text-white border' >My project </motion.a>
+                        <motion.a 
+                          initial="hidden"
+                        animate="show"
+                        variants={fadeInUpAnimation}
+                        transition={{
+                            delay:1
+                        }
+                        }
+                        href="#Contact" className='px-5 py-2 bg-transparent border border-white rounded-full text-white'>contact</motion.a>
                     </div>
                 </div>
             </div>
